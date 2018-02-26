2dbc41bfe0398b007c337c69814f6b19c31c5bf1
ING MENDOZA <flapon22@gmail.com>
2018-02-26 11:30:07 -0600

    proyecto vue-laravel-crud

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,45 @@
+@extends('app')
+
+@section('content')
+
+	<div id="crud" class="row">
+		<div class="col">
+			<h1 class="page-header"> CRUD Laravel y VUEjs</h1>
+
+			<div class="row">
+				<div class="col-sm-7">
+					<a href="#" class="btn btn-primary float-right" data-toggle="modal" data-target="#create">Nueva Tarea</a>
+					<table class="table table-hover table-striped">
+						<thead>
+							<tr>
+								<th>ID</th>
+								<th>Tarea</th>
+								<th colspan="2">&nbsp;</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr v-for="keep in keeps">
+								<td width="10px">@{{ keep.id }}</td>
+								<td>@{{ keep.keep }}</td>
+								<td width="10px">
+									<a href="#" class="btn btn-warning bth-sm" v-on:click.prevent="editKeep(keep)">Editar</a>
+								</td>
+								<td width="10px">
+									<a href="#" class="btn btn-danger bth-sm" v-on:click.prevent="deleteKeep(keep)">Eliminar</a>
+								</td>
+							</tr>
+						</tbody>
+					</table>
+					@include('create')
+					@include('edit')
+				</div>
+				<div class="col-sm-5">
+					<pre>
+						@{{ keeps }}
+					</pre>
+				</div>
+			</div>	
+		</div>
+	</div>
+
+@endsection
